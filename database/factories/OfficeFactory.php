b17c8bdcbf0fc26eca4d614ff331ce4c049ee4e0
esakimedia <apachecordovax@gmail.com>
2022-04-27 20:56:02 +0200

    prepare models

--- database/factories/OfficeFactory.php
+++ database/factories/OfficeFactory.php
@@ -2,11 +2,19 @@
 
 namespace Database\Factories;
 
+use App\Models\Office;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OfficeFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     * 
+     * @var string
+     */
+    protected $model = Office::class;
+
     /**
      * Define the model's default state.
      *
--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -3,10 +3,19 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Office;
+use App\Models\Reservation;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ReservationFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     * 
+     * @var string
+     */
+    protected $model = Reservation::class;
+
     /**
      * Define the model's default state.
      *
