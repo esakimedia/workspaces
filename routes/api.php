94e9724ec760e52bfd0bf8818771aabaf6e86694
esakimedia <apachecordovax@gmail.com>
2022-04-28 06:20:38 +0200

    lists all offices in paginated way

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\OfficeController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 // Tags...
 Route::get('/tags', TagController::class);
+
+// Offices...
+Route::get('/offices', [OfficeController::class, 'index']);
---
+++ tests/Feature/OfficeControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Office;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class OfficeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_lists_all_offices_in_paginated_way()
+    {
+        Office::factory()->count(3)->create();
+
+        $response = $this->get('/api/offices');
+
+        $response->assertOk();
+        $response->assertJsonCount(3, 'data');
+        $this->assertNotNull($response->json('data')[0]['id']);
+        $this->assertNotNull($response->json('meta'));
+        $this->assertNotNull($response->json('links'));
+    }
+}
