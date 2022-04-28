c4a695d777608e9aad30db64e6af5600ea826dce
esakimedia <apachecordovax@gmail.com>
2022-04-28 07:01:55 +0200

    filters by user id

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\OfficeResource;
 use App\Models\Office;
+use App\Http\Resources\OfficeResource;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class OfficeController extends Controller
@@ -13,7 +14,16 @@ class OfficeController extends Controller
         $offices = Office::query()
             ->where('approval_status', Office::APPROVAL_APPROVED)
             ->where('hidden', false)
-            ->when(request('host_id'), fn ($builder) => $builder->whereUserId(request('host_id')))
+            ->when(
+                request('host_id'),
+                fn ($builder)
+                => $builder->whereUserId(request('host_id'))
+            )
+            ->when(
+                request('user_id'),
+                fn (Builder $builder)
+                => $builder->whereRelation('reservations', 'user_id', '=', request('user_id'))
+            )
             ->latest('id')
             ->paginate(20);
 
